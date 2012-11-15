320057ad527b6cfd1cadbaad46b079783b50edd7
piyuesh23 <piyuesh23@qed42.com>
2012-11-15 20:52:01 +0530

    heroku mongodb

--- config/config.js
+++ config/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  db: 'mongodb://localhost:27017/noobjs_dev'
+  // db: 'mongodb://localhost:27017/noobjs_dev'
+  db: 'mongodb://piyuesh23:9341255505pk@mars.mongohq.com:10012/app9241069'
 }
