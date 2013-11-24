22a600050de044f551b5ac59af2878a69f932f22
piyuesh23 <piyuesh23@qed42.com>
2013-11-24 17:45:04 +0530

    mongodb credentials

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   'development' : {
     db: 'mongodb://localhost:27017/noobjs_dev'
-  }, 
+  },
   'production' : {
-    db: 'mongodb://piyuesh23:9341255505pk@alex.mongohq.com:10096/app9241069'
+    db: 'mongodb://piyuesh23:9341255505pk@paulo.mongohq.com:10073/app19765053'
   }
 }
